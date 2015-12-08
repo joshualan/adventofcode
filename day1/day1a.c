901f16e2470594884be79de8e5ffc779aa49ec95
Alan Estrada <joshualan23@yahoo.com>
2015-12-07 21:32:06 -0500

    Added solutions to Day 1! HOLLA!

---
+++ day1/day1a.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  FILE *fp;
+  char c;
+  int floor = 0;
+
+  if (argc != 2) {
+    printf( "Incorrect # of args.\n");
+  }
+
+  fp = fopen(argv[1], "r");
+
+  if (!fp) {
+    printf("File not found\n");
+    return 0;
+  }
+
+  while (fscanf(fp, "%c", &c) != EOF) {
+    if (c == '(') {
+      floor++;
+    }
+    else if (c == ')')  {
+      floor--;
+    }
+  }
+
+  printf( "Santa's ends up at floor %d!\n", floor);
+  return 0;
+
+}
---
+++ day1/day1b.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  FILE *fp;
+  char c;
+  int floor = 0, count = 1;
+
+  if (argc != 2) {
+    printf( "Incorrect # of args.\n");
+  }
+
+  fp = fopen(argv[1], "r");
+
+  if (!fp) {
+    printf("File not found\n");
+    return 0;
+  }
+
+  while (fscanf(fp, "%c", &c) != EOF) {
+    if (c == '(') {
+      floor++;
+    }
+    else if (c == ')')  {
+      floor--;
+    }
+
+    if (count && floor == -1) {
+      printf("Santa first enters the basement at input %d. Is he gonna fight Krampus?!\n", count);
+      count = 0;
+    }
+    
+    if (count){
+      count++;
+    }
+  }
+
+  printf( "Santa's ends up at floor %d!\n", floor);
+  return 0;
+
+}
