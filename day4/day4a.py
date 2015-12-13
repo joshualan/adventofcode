b7c2aebbd9e4d36b34644b2496df8e4d0739605d
Alan Estrada <joshualan23@yahoo.com>
2015-12-13 00:12:31 -0500

    Added day 4 solutions!

---
+++ day4/day4a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""day4a.py: Solution for the Day 4A puzzle.
+
+I used Python instead of C because of easy md5 hashing
+functionality.
+
+"""
+
+__author__      = "Alan Estrada"
+
+import hashlib
+
+currentNum = 0
+input = "ckczppom"
+
+while True:
+    # Calculate the hash of input and currentNum 'til the first
+    # 5 characters are "00000"
+    hash_object = hashlib.md5(''.join([input,str(currentNum)]))
+    
+    if hash_object.hexdigest()[:5]  == "00000":
+        print currentNum
+        break
+
+    currentNum += 1
+    
+
